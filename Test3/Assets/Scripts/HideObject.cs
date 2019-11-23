b2b7f46a227aff78aee3bb52cdc3a7fdd5aa0706
siyel4512 <siyel4512@naver.com>
2019-11-24 01:36:13 +0900

    Update Test3 project [2019.11.24]

--- Test3/Assets/Scripts/HideObject.cs
+++ Test3/Assets/Scripts/HideObject.cs
@@ -6,6 +6,8 @@ public class HideObject : MonoBehaviour {
 
     [Header("숨길 오브젝트")]
     [SerializeField] GameObject hideObject;
+    [Header("표시할 오브젝트")]
+    [SerializeField] GameObject showObject;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +15,7 @@ public class HideObject : MonoBehaviour {
         {
             //Debug.Log("숨기기");
             hideObject.SetActive(false);
+            showObject.SetActive(true);
             //Debug.Log(other.name);
         }
     }
@@ -23,6 +26,7 @@ public class HideObject : MonoBehaviour {
         {
             //Debug.Log("숨김 해제");
             hideObject.SetActive(true);
+            showObject.SetActive(false);
             //Debug.Log(other.name);
         }
     }
---
+++ Test3/Assets/Scripts/LightController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    Renderer rend;
+    Material mat;
+    [Header("라이트 밝기 조절")][Range(0,1)]
+    [SerializeField] float lightValue;
+    [Header("색상 선택")][Tooltip("1. Nomal Color, 2. White, 3. Cyan, 4. Red, 5. Pink, 6. Yellow, 7. Green, 8. Blue")]
+    [SerializeField] int selectColor = 1;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+    }
+
+    void Update() {
+        SelectLightColor(selectColor);
+        mat.SetColor("_EmissionColor", mat.color * lightValue);
+    }
+
+    void SelectLightColor(int _lightColor)
+    {
+        switch (_lightColor)
+        {
+            case 1:
+                break;
+            case 2:
+                mat.color = Color.white;
+                break;
+            case 3:
+                mat.color = Color.cyan;
+                break;
+            case 4:
+                mat.color = Color.red;
+                break;
+            case 5:
+                mat.color = new Color(255, 0, 225, 225);
+                break;
+            case 6:
+                mat.color = Color.yellow;
+                break;
+            case 7:
+                mat.color = Color.green;
+                break;
+            case 8:
+                mat.color = Color.blue;
+                //mat.color = new Color(255, 106, 0, 255);
+                break;
+        }
+    }
+}
